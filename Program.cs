cf1d2d05032e82ad652a3e793851337cc2d77465
Divya Zinzuvadia <divya@omsinv.com>
2024-04-10 22:23:59 +0530

    WIP

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Domaincasepro.Queries;
+using Domaincasepro.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Dependency Injections
+var conString = builder.Configuration.GetSection("ConnectionStrings").GetSection("ConnectionString").Value;
+builder.Services.AddDbContext<Modelcasepro.Entities.CaseproDbContext>(options =>
+                options.UseSqlServer(conString));
+builder.Services.AddTransient<IActivityRepository, ActivityRepository>();
+builder.Services.AddScoped<ActivityQueryHandler>();
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
